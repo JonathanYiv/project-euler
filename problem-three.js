05329ce41a813ad8ab4cfb840aae6b68794255fe
Jonathan Yiv <JonathanYiv@gmail.com>
2017-07-23 22:57:00 -0400

    Solved problem three in JS. Tried to convert solution to ruby.. currently an infinite loop..

--- problem-three.js
+++ problem-three.js
@@ -1,11 +1,41 @@
-var number = 54;
+// This right here verifies that a number is prime or not prime!
+function verifyPrime(number) {
 
-for(var i = 2; i < number; i++) {
-  if(number % i === 0) {
-    console.log('not prime!');
-  }
+	for(var i = 2; i < number; i++) {
+
+  		if(number % i === 0) {
+
+    		return false;
+
+ 		 } 
+
+ 		 return true;
+
+	}
 }
 
-// Currently unsolved!
-// Testing git SSH authentication
+function largestPrimeFactor(number) {
 
+	var factor;
+
+	for(var i = 0; i <= number; i++) {
+
+		if( Number.isInteger(number / i) ) {
+
+			if ( verifyPrime(i) ) {
+
+				number = number / i;
+
+				factor = i;
+
+			}
+
+		}
+
+	}
+
+	return factor;
+
+}
+
+console.log(largestPrimeFactor(600851475143));
--- problem-three.rb
+++ problem-three.rb
@@ -1,1 +1,32 @@
 # duly note: I am not strong on blocks, procs, and methods that call them. Need more practice
+
+
+require 'prime'
+
+def largestPrimeFactor(number)
+
+	factor = nil
+
+	for i in 1..number
+
+		if( (number / i).is_a? Integer ) 
+
+			if Prime.prime?(i) 
+
+				number = number / i
+
+				factor = i
+
+			end
+
+		end
+
+	end
+
+	return factor;
+
+end
+
+puts largestPrimeFactor(600851475143)
+
+# still unsolved! Hitting an infinite loop somewhere.
